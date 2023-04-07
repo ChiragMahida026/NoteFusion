68a273b3ad1e6300771539fef42e9ceeb5b43cad
Chirag Mahida <chirag.mahida@scikey.ai>
2023-04-08 01:39:45 +0530

    Changes

--- models/src/main/resources/db/migration/release_static_data/V0.0.1__Controltbl_insert_data.sql
+++
@@ -1,9 +0,0 @@
-insert into roughsync.Hcis2Rough_DATASYNC_MASTER (TABLE_NAME,P2L_LOCAL_FILL,P2L_REMOTE_TABLE,ACTIVE,EXEC_SEQUENCE,CONTROL_TABLE_NAME)
-values
-('HumanResources_division','roughsync.Hcis2Rough_TRAN_HumanResources_division_Data','roughsync.H2R_HumanResources_division_REMOTE',1,1,'HumanResources_division_h2r');
-GO
-insert into roughsync.Hcis2Rough_CONTROL_TABLE (TABLENAME,TABLEIDNNO,MODIFYDATETIME,LOCAL_TABLENAME,IS_ACTIVE,EXEC_SEQUENCE)
-values
-('HumanResources_division_h2r',0,GETDATE(),'roughsync.H2R_HumanResources_division_LOCAL',1,1);
-GO
-
--- models/src/main/resources/db/migration/release_v1.0/V0.0.1__Create-Schema.sql
+++
@@ -1,5 +0,0 @@
-IF NOT EXISTS ( SELECT top 1 1 FROM sys.schemas WHERE name = N'roughsync' )
-BEGIN
-EXEC('CREATE SCHEMA [roughsync]')
-END
-GO
